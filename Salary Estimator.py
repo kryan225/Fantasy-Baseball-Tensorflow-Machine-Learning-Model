71a9ecfda3aff269eca959d0ac437fff07e04a41
DESKTOP-SUKUVBQ\kryan <kryan225.gomets@gmail.com>
2019-02-04 12:46:31 -0800

    Adding estimator file - only comments no code yet

---
+++ Salary Estimator.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  4 12:44:10 2019
+
+This file's purpose is to train a nueral net using tensorflow. The NN will be able to make predictions
+for a baseabll player's fantasy baseball salary value, based off given projected stats
+
+This is for a personal project where I develop a fantasy baseball draft toolkit for my custom league. 
+The winner of my league gets $600+ and as of developing this, my team has finished 7th place four years in a row.
+The cutoff of winning any money back is 6th place so my co-owner/brother and I are a little annoyed. I hope that this
+toolkit will provide us with an advantage for evaluating players and forming a draft strategy. Even though you don't necessarily 
+win your league on draft day, you can have a massive head start and it is possible to lose the league at the draft. 
+
+any questions or comments please email me at: kryan225.gomets@gmail.com
+
+@author: kryan
+"""
+
